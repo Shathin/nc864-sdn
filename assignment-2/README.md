9eb71fd33c4cef00a8e25cd5bdb0c8fbd0460733
Shathin <shathin.rao@gmail.com>
2021-09-11 16:13:01 +0530

    ✅ completed assignment 2

---
+++ assignment-2/README.md
@@ -0,0 +1,46 @@
+# Assignment 2 - Routing Logic (IP Forwarding Mechanism) in Mininet
+
+## Problem Statement
+
+_[View problem statement](./NC864_Assignment_2.pdf)_
+
+## Reports
+
+Read the report _[here](./Report.pdf)_ or  on _[Notion](https://shathin.notion.site/NC864-SDN-Assignment-2-0c6d4cdd7d5f4879ae14a2806014828c)._
+
+## Target Network
+
+![Target Network consisting of 6 hosts, 2 switches and 4 routers.](./misc/Target-Network.png)
+
+The above diagram shows the target networks with the additional neccessary assumptions made based on the target network shown in the problem statement. 
+
+Following are the different subnets and hosts in those networks - 
+
+- $10.0.1.0/24$ → $R1-eth0$, $H12$, $H13$ and $S1$.
+- $10.0.2.0/24$ → $R2-eth0$ and $H22$
+- $10.0.3.0/24$ → $R3-eth0$ and $H32$
+- $10.0.4.0/24$ → $R4-eth0$, $H42$, $H43$ and $S2$.
+- $192.168.0.0/24$ → $R1-eth1$ and $R3-eth1$
+- $192.168.1.0/24$ → $R1-eth2$ and $R2-eth2$
+- $192.168.2.0/24$ → $R2-eth1$ and $R4-eth1$
+
+## Scripts
+
+Two scripts are available for execution `assignment2_routing1.py` and `assignment2_routing2.py`.
+
+```bash
+sudo python3 script.py
+```
+
+Both the scripts create the same target network but differ in the connectivity provided for the hosts in the network.
+
+### `assignment2_routing1.py`
+
+This scripts creates the target network and allows all hosts $H12$. $H13$. $H22$, $H32$, $H42$ and $H43$ to be able to ping each other.
+
+### `assignment2_routing2.py`
+
+This script creates the target network and allows hosts $H12$. $H13$. $H22$, $H32$, $H42$ and $H43$ to be able to ping each other with the exception of $H12$ being able to ping $H22$ (and vice-versa).
+
+
+Be sure to clean up before executing the script for another time. Run `sudo mn -c` command to perform the cleanup.
---
+++ assignment-2/scripts/assignment2_routing1.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Node, OVSSwitch, DefaultController
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
+
+class LinuxRouter(Node):
+    def config(self, **params):
+        super(LinuxRouter, self).config(**params)
+        self.cmd("sysctl net.ipv4.ip_forward=1")
+
+    def terminate(self):
+        self.cmd("sysctl net.ipv4.ip_forward=0")
+        super(LinuxRouter, self).terminate()
+
+
+class RouterTopology(Topo):
+    def build(self, **_opts):
+        info("\n*** Creating topology\n")
+        # * Add 4 routers
+        info("*** *** Adding routers\n")
+        r1 = self.addNode("r1", cls=LinuxRouter, ip="10.0.1.1/24")
+        r2 = self.addNode("r2", cls=LinuxRouter, ip="10.0.2.1/24")
+        r3 = self.addNode("r3", cls=LinuxRouter, ip="10.0.3.1/24")
+        r4 = self.addNode("r4", cls=LinuxRouter, ip="10.0.4.1/24")
+
+        # * Create network under r1
+        info("*** *** Adding network under Router r1\n")
+        s1 = self.addSwitch("s1")
+        self.addLink(s1, r1, intfName2="r1-eth0", params2={"ip": "10.0.1.1/24"})
+        h12 = self.addHost(name="h12", ip="10.0.1.2/24", defaultRoute="via 10.0.1.1")
+        h13 = self.addHost(name="h13", ip="10.0.1.3/24", defaultRoute="via 10.0.1.1")
+        self.addLink(s1, h12)
+        self.addLink(s1, h13)
+
+        # * Create network under r2
+        info("*** *** Adding network under Router r2\n")
+        h22 = self.addHost(name="h22", ip="10.0.2.2/24", defaultRoute="via 10.0.2.1")
+        self.addLink(h22, r2, intfName2="r2-eth0", params2={"ip": "10.0.2.1/24"})
+
+        # * Create network under r3
+        info("*** *** Adding network under Router r3\n")
+        h32 = self.addHost(name="h32", ip="10.0.3.2/24", defaultRoute="via 10.0.3.1")
+        self.addLink(h32, r3, intfName2="r3-eth0", params2={"ip": "10.0.3.1/24"})
+
+        # * Create network under r4
+        info("*** *** Adding network under Router r4\n")
+        s2 = self.addSwitch("s2")
+        self.addLink(s2, r4, intfName2="r4-eth0", params2={"ip": "10.0.4.1/24"})
+        h42 = self.addHost(name="h42", ip="10.0.4.2/24", defaultRoute="via 10.0.4.1")
+        h43 = self.addHost(name="h43", ip="10.0.4.3/24", defaultRoute="via 10.0.4.1")
+        self.addLink(s2, h42)
+        self.addLink(s2, h43)
+
+        # * Connect all routers
+        info("*** *** Connecting routers\n")
+        self.addLink(
+            r1,
+            r3,
+            intfName1="r1-eth1",
+            intfName2="r3-eth1",
+            params1={"ip": "192.168.0.1/24"},
+            params2={"ip": "192.168.0.3/24"},
+        )
+        self.addLink(
+            r2,
+            r4,
+            intfName1="r2-eth1",
+            intfName2="r4-eth1",
+            params1={"ip": "192.168.2.2/24"},
+            params2={"ip": "192.168.2.4/24"},
+        )
+        self.addLink(
+            r1,
+            r2,
+            intfName1="r1-eth2",
+            intfName2="r2-eth2",
+            params1={"ip": "192.168.1.1/24"},
+            params2={"ip": "192.168.1.2/24"},
+        )
+
+        info("*** Topology creation complete\n")
+
+
+setLogLevel("info")
+
+net: Mininet = Mininet(topo=RouterTopology())
+
+# * Start mininet network
+net.start()
+
+pingTimeout: float = 0.25
+
+info("\n*** Testing connectivity before setting up routing rules\n")
+net.pingAll(timeout=pingTimeout)  # ? Timeout in seconds
+
+# * Setup routing rules
+info("\n*** Setup routing rules\n")
+# ? ip route add dest_network via dest_network_outer_ip dev src_net_outer_intf
+net["r1"].cmd("ip route add 10.0.2.0/24 via 192.168.1.2")  # * r1 -> r2
+net["r1"].cmd("ip route add 10.0.3.0/24 via 192.168.0.3")  # * r1 -> r3
+net["r1"].cmd("ip route add 10.0.4.0/24 via 192.168.1.2")  # * r1 -> r4
+
+net["r2"].cmd("ip route add 10.0.1.0/24 via 192.168.1.1")  # * r2 -> r1
+net["r2"].cmd("ip route add 10.0.3.0/24 via 192.168.1.1")  # * r2 -> r3
+net["r2"].cmd("ip route add 10.0.4.0/24 via 192.168.2.4")  # * r2 -> r4
+
+net["r3"].cmd("ip route add 10.0.1.0/24 via 192.168.0.1")  # * r3 -> r1
+net["r3"].cmd("ip route add 10.0.2.0/24 via 192.168.0.1")  # * r3 -> r2
+net["r3"].cmd("ip route add 10.0.4.0/24 via 192.168.0.1")  # * r3 -> r4
+
+net["r4"].cmd("ip route add 10.0.1.0/24 via 192.168.2.2")  # * r4 -> r1
+net["r4"].cmd("ip route add 10.0.2.0/24 via 192.168.2.2")  # * r4 -> r2
+net["r4"].cmd("ip route add 10.0.3.0/24 via 192.168.2.2")  # * r4 -> r3
+
+
+info("\n*** Testing connectivity after setting up routing rules\n")
+net.pingAll(timeout=pingTimeout)
+
+# * Transfer control to command line interface
+CLI(net)
+
+# * Stop network
+net.stop()
